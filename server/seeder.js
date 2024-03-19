a3dd1f0f49c7753b99b00f1e78c472f4e5a3372e
Satria-WS <extencezone02@gmail.com>
2024-03-19 21:57:55 +0700

    add routes product

--- server/seeder.js
+++ server/seeder.js
@@ -9,47 +9,64 @@ import { User } from "./models/userModel.js";
 import connectDb from "./config/db.js";
 
 dotenv.config();
+
 connectDb();
 
+// Function to import data
 const importData = async () => {
   try {
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
+    // Clear existing data
+    await clearData();
 
+    // Insert users into the database
     const createdUsers = await User.insertMany(users);
-
     const adminUser = createdUsers[0]._id;
 
-    const sampleProducts = products.map((product) => {
-      return { ...product, user: adminUser };
-    });
+    // Add admin user to products
+    const sampleProducts = products.map((product) => ({
+      ...product,
+      user: adminUser,
+    }));
 
+    // Insert products into the database
     await Product.insertMany(sampleProducts);
 
-    console.log('Data Imported!'.green.inverse);
+    console.log("Data Imported!".green.inverse);
     process.exit();
   } catch (error) {
     console.error(`${error}`.red.inverse);
+    handleFailure(error, "Data import failed!");
     process.exit(1);
   }
 };
+// Function to clear existing data
+const clearData = async () => {
+  await Order.deleteMany();
+  await Product.deleteMany();
+  await User.deleteMany();
+};
 
+// Function to destroy data
 const destroyData = async () => {
   try {
-    await Order.deleteMany();
-    await Product.deleteMany();
-    await User.deleteMany();
-
-    console.log('Data Destroyed!'.red.inverse);
+    await clearData();
+    console.log("Data Destroyed!".red.inverse);
     process.exit();
   } catch (error) {
     console.error(`${error}`.red.inverse);
+    handleFailure(error, "Data destruction failed!");
     process.exit(1);
   }
 };
 
-if (process.argv[2] === '-d') {
+// Handle error and exit process
+const handleFailure = (error, message) => {
+  console.error(`${message}: ${error.message}`.red.inverse);
+  process.exit(1);
+};
+
+// Check command line arguments and execute appropriate action
+if (process.argv[2] === "-d") {
   destroyData();
 } else {
   importData();
