fbcecf084b4063eb9c3d3309a5ab4e5ff0106707
Satria-WS <extencezone02@gmail.com>
2024-03-21 13:46:11 +0700

    adding error handling middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDb from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { errorHandler,notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -17,7 +18,9 @@ app.get('/',(req,res) => {
   res.send('API is running...');
 })
 
-app.use('/api/products',productRoutes)
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 // app.get("/", (req, res) => {
 //   res.send("API is running...");
 // });
