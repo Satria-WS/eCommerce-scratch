64d82580948310136f77bade1733c8a357ef0f5b
Satria-WS <extencezone02@gmail.com>
2024-03-21 14:46:48 +0700

    add comment for controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,11 +1,18 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import { Product } from "../models/productModel.js";
 
+
+// @desc fetch all products
+// @route Get /api/products
+// @access Public
 export const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json(products);
 });
 
+// @desc fetch all products
+// @route Get /api/products/:id
+// @access Public
 export const getProductsById = asyncHandler(async (req, res) => {
   const products = await Product.findById(req.params.id);
   if (!products) {
