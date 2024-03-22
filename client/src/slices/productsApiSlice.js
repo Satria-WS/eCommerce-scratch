18632dde4346f72c418a96ab7c993d60d3396ed3
Satria-WS <extencezone02@gmail.com>
2024-03-22 10:47:23 +0700

    making product slice api

---
+++ client/src/slices/productsApiSlice.js
@@ -0,0 +1,11 @@
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({ url: PRODUCTS_URL }),
+    }),
+    keepUnusedDatafor: 5,
+  }),
+});
