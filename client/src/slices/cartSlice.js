f31035149ff551e10a45f7417b5d84c7b257ef48
Satria-WS <extencezone02@gmail.com>
2024-03-24 13:03:48 +0700

    complete calculate

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -4,6 +4,9 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2); //example 7.40
+};
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -19,9 +22,21 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item];
       }
       //Calculate  items price
-      state.itemsPrice = state.cartItems.reduce(
-        (acc, item) => acc + item.price * item
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item)
       );
+      //Calculate if order over $100 is free , else $10 shipping
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      //Calculate tax price price  15% tax
+      state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2));
+
+      //Calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
     },
   },
 });
