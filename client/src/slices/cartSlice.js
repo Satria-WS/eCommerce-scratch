b65412a3dc041fedacc8a359abc330fd794bf4b7
Satria-WS <extencezone02@gmail.com>
2024-03-25 09:03:42 +0700

    fixing decimal and clean

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -1,13 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = {
   cartItems: localStorage.getItem("cart")
     ? JSON.parse(localStorage.getItem("cart"))
     : [],
-};
-
-const addDecimals = (num) => {
-  return (Math.round(num * 100) / 100).toFixed(2);
 };
 
 const cartSlice = createSlice({
@@ -24,24 +21,7 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-      //calculate items price
-      state.itemsPrice = state.cartItems.reduce(
-        (acc, item) => acc + item.price * item.qty,
-        0
-      );
-      //calculate shipping price
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-      //calculate tax price
-      state.taxPrice = addDecimals(
-        Number((0.15 * state.itemsPrice).toFixed(2))
-      );
-      //calculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
