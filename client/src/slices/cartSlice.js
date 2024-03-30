572780d703c10f890f1326e1911f2eea72e51a2f
Satria-WS <extencezone02@gmail.com>
2024-03-30 22:33:14 +0700

    refactor cartSlice

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -2,9 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
 
 const initialState = {
-  cartItems: localStorage.getItem("cart")
-    ? JSON.parse(localStorage.getItem("cart"))
-    : [],
+  cartItems: JSON.parse(localStorage.getItem("cart")) || [],
 };
 
 const cartSlice = createSlice({
@@ -13,22 +11,20 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const item = action.payload;
-      const existItem = state.cartItems.find((x) => x._id === item._id);
-      if (existItem) {
-        state.cartItems = state.cartItems.map((x) =>
-          x._id === existItem._id ? item : x
-        );
+      const existItemIndex = state.cartItems.findIndex((x) => x._id === item._id);
+      if (existItemIndex !== -1) {
+        state.cartItems[existItemIndex] = item;
       } else {
-        state.cartItems = [...state.cartItems, item];
+        state.cartItems.push(item);
       }
-      return updateCart(state);
+      updateCart(state);
     },
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
-      return updateCart(state);
+      updateCart(state);
     }
-
   },
 });
-export const { addToCart , removeFromCart } = cartSlice.actions;
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
